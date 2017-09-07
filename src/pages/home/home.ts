e0ee98920711dcc33d5fbfd540c9432af6af71b9
Martin Schenck <martinschenck@fastmail.com>
2017-09-07 22:41:12 +0200

    Added location input and storage
    The home page now has an input at the top where a location name can be
    given.
    
    Local storage is added and stores the location at the press of a button.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+import { Location } from '../../app/location';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  location: Location = new Location();
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private storage: Storage) {
+    storage.get('location').then((locationName) => {
+      this.location.name = locationName;
+    });
   }
 
+  setLocation() {
+    this.storage.set('location', this.location.name)
+      .then(function() {console.log('Stored location name')})
+      .catch(function() {console.log('Could not store location name')});
+  }
 }
